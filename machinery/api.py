207ed7fa6c1e0deb54cf72b4c87dca983366ba66
Theo Bob Massard <tbobm@protonmail.com>
2021-11-26 23:15:41 +0100

    feat(api): create a basic http health route
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- machinery/api.py
+++ machinery/api.py
@@ -1,1 +1,21 @@
 """Main module."""
+import os
+
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route('/healthcheck')
+def healthcheck():
+    """Simply return OK."""
+    return 204
+
+
+def main():
+    """Start the Flask application."""
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', "5000")))
+
+
+if __name__ == "__main__":
+    main()
