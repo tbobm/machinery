4cba220d2aabfa016a22aa666f7803c3eea6bee6
Theo Bob Massard <tbobm@protonmail.com>
2021-11-27 16:03:11 +0100

    feat: create basic `/w` POST route
    Allow simple Workflow creation, no error checking or validation
    whatsoever.
    
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- tests/test_api.py
+++ tests/test_api.py
@@ -9,3 +9,10 @@ def test_create_service(client, valid_service):
     rv = client.post('/s', json=valid_service)
 
     assert rv.status_code == 201
+
+
+def test_create_workflow(client, valid_workflow):
+    rv = client.post('/w', json=valid_workflow)
+
+    assert "workflow_id" in rv.get_json()
+    assert rv.status_code == 201
