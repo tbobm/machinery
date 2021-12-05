c636f701f5f2c11eb05a733e580a1c92e63ddff8
Theo Bob Massard <tbobm@protonmail.com>
2021-12-05 20:39:26 +0100

    tests: differiante entity name for each test
    Signed-off-by: Theo Bob Massard <tbobm@protonmail.com>

--- tests/test_db.py
+++ tests/test_db.py
@@ -7,13 +7,17 @@ from machinery import db
 
 def test_insert_workflow(valid_workflow, db_client):
     """Ensure a Valid Workflow can be inserted in the proper Collection."""
-    created, data = db.store_workflow(valid_workflow, db_client)
+    workflow = {
+        **valid_workflow,
+        **{'name': 'valid-insert'},
+    }
+    created, data = db.store_workflow(workflow, db_client)
     assert created is True
     assert 'workflow_id' in data
     database = db_client.get_default_database()
     query = {"_id": ObjectId(data['workflow_id'])}
     doc = database[db.MachineryCollections.WORKFLOW.value].find_one(query)
-    assert doc['name'] == valid_workflow['name']
+    assert doc['name'] == workflow['name']
 
 
 def test_insert_workflow_failed(bad_generic_payload, db_client):
@@ -33,13 +37,17 @@ def test_insert_workflow_failed_no_service(valid_workflow, db_client):
 
 def test_insert_service(valid_service, db_client):
     """Ensure a Valid Service can be inserted in the proper Collection."""
-    created, data = db.store_service(valid_service, db_client)
+    service = {
+        **valid_service,
+        **{'name': 'valid-insert'},
+    }
+    created, data = db.store_service(service, db_client)
     assert created is True
     assert 'service_id' in data
     database = db_client.get_default_database()
     query = {"_id": ObjectId(data['service_id'])}
     doc = database[db.MachineryCollections.SERVICE.value].find_one(query)
-    assert doc['name'] == valid_service['name']
+    assert doc['name'] == service['name']
 
 
 def test_insert_service_failed(bad_generic_payload, db_client):
@@ -51,10 +59,14 @@ def test_insert_service_failed(bad_generic_payload, db_client):
 
 def test_service_lookup(valid_service, db_client):
     """Ensure service lookup returns True if services are found."""
-    created, _ = db.store_service(valid_service, db_client)
+    service = {
+        **valid_service,
+        **{'name': 'lookup'},
+    }
+    created, _ = db.store_service(service, db_client)
     assert created is True
 
-    found = db.services_exists([valid_service["name"]], db_client)
+    found = db.services_exists([service["name"]], db_client)
     assert found is True
 
 
